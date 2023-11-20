8798558304b013d8426abb00f2969aa85b3b5708
Mark Oudshoorn <markoudshoorn123@gmail.com>
2023-11-20 08:36:11 +0100

    Week 1
    documentatie & start frontpage

---
+++ IngsocIncidents/Global/Start.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ingsoc</title>
+    <link rel="stylesheet" href="Styles.css">
+</head>
+<body>
+<div id="IngsocLogo">
+    <img id=IngsocLogo-img src="Resources/Ingsoc-logo.PNG" alt="Ingsoc logo">
+</div>
+<div id="Start_menu">
+    <button id="Start_button1">Klacht indienen</button>
+
+
+</div>
+</body>
+</html>
